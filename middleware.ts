84e2b82f9a460291c804a38af5eed8b56a6442cc
depermana12 <deddiapermana97@gmail.com>
2024-07-04 22:05:33 +0700

    middleware to protect route

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,12 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
+
+export default auth((req) => {
+  if (!req.auth) {
+    const newUrl = new URL("/api/auth/signin", req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+});
+
+export const config = {
+  matcher: ["/users/:id*"],
+};
